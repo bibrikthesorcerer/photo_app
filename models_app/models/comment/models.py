a608bfd86c41059ec9f54f105a796ab51ac5d367
bibrikthesorcerer <ignatsahovsky@gmail.com>
2024-11-14 18:40:18 +0300

    altered Comment model

--- models_app/models/comment/models.py
+++ models_app/models/comment/models.py
@@ -6,7 +6,7 @@ class Comment(MetaAbstract, SoftDelMixin):
     user = models.ForeignKey('models_app.UserProfile', on_delete=models.CASCADE)
     photo = models.ForeignKey('models_app.Photo', on_delete=models.CASCADE)
     text = models.CharField(max_length=256)
-    parent = models.ForeignKey('self', on_delete=models.DO_NOTHING, related_name='children',null=True)
+    parent = models.ForeignKey('self', on_delete=models.DO_NOTHING, related_name='children', null=True, default=None, blank=True)
     pub_date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
