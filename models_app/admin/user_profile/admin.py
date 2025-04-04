26b83a6b1c79f6e4db83e691cd11c525275fa87c
bibrikthesorcerer <ignatsahovsky@gmail.com>
2025-04-04 13:33:35 +0300

    Moved notifications in admin to userProfile change_list, added modal to input text

--- models_app/admin/user_profile/admin.py
+++ models_app/admin/user_profile/admin.py
@@ -6,6 +6,7 @@ from django.shortcuts import redirect
 
 from .forms import UserProfileForm, AdminUserProfileForm
 from models_app.models import UserProfile
+from notifications.utils import send_message_to_list_of_users
 
 
 @admin.register(UserProfile)
@@ -13,7 +14,17 @@ class UserProfileAdmin(admin.ModelAdmin):
     list_display = ["username", "role", "id", "email", "first_name", "last_name", "last_login"]
     search_fields = list_display
     list_filter = ["role", "last_login", "updated_at"]
+    actions = ['send_notification']
+    change_list_template = 'admin/models_app/user_profile/change_list.html'
 
+    @admin.action(description="Send notification to list of users")
+    def send_notification(self, request, queryset):
+        if 'message' in request.POST:
+            message = request.POST['message']
+            ids = queryset.values_list('pk', flat=True)
+            send_message_to_list_of_users(ids, 'send_notification', message)
+            self.message_user(request, f"Message sent to {queryset.count()} users")
+        return redirect(request.get_full_path())
 
     def get_form(self, request, obj=None, **kwargs):
         if (request.user.role == UserProfile.ROLE_ADMIN 
