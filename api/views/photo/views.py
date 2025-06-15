bb796a3197615091783c07670ffff622595830dd
bibrikthesorcerer <ignatsahovsky@gmail.com>
2025-06-15 15:17:13 +0300

    added basic docs to a SinglePhotoView using OpenApi's @extend_schema

--- api/views/photo/views.py
+++ api/views/photo/views.py
@@ -2,6 +2,8 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework import status
 from service_objects.services import ServiceOutcome
+from drf_spectacular.utils import extend_schema, OpenApiParameter, OpenApiResponse
+from drf_spectacular.types import OpenApiTypes
 
 from api.views.base_view import BaseView
 from api.services import ListPhotos, CreatePhoto, RetrievePhoto, UpdatePhoto, SchedulePhotoDeletion, RecoverPhotoFromDeletion
@@ -47,6 +49,19 @@ class SinglePhotoView(BaseView):
         self.check_object_permissions(self.request, object)
         return object
 
+    @extend_schema(
+        summary="Get single Photo using id.",
+        parameters=[
+            OpenApiParameter(name="photo_id", location="path", type=int, description="id of Photo to be retrieved"),
+            OpenApiParameter(name="user", location="query", required=False, type=int,
+                             description="id of user, used to add user actions on Photo instance (e.g. is_liked)"
+            )
+        ],
+        responses={
+            200: PhotoSerializer,
+            404: OpenApiResponse(description="Photo with given id not found")
+        },
+    )
     def get(self, request, *args, **kwargs):
         outcome = ServiceOutcome(
             RetrievePhoto,
@@ -55,6 +70,22 @@ class SinglePhotoView(BaseView):
         data = PhotoSerializer(outcome.result).data
         return Response(data, status=status.HTTP_200_OK)
 
+    @extend_schema(
+        summary="Update Photo's information.",
+        parameters=[
+            OpenApiParameter(name="photo_id", location="path", type=int, description="id of Photo to be updated"),
+        ],
+        request={
+            "img": OpenApiTypes.BINARY,
+            "title": OpenApiTypes.STR,
+            "description": OpenApiTypes.STR
+        },
+        responses={
+            200: PhotoSerializer,
+            400: OpenApiResponse(description="No values for new fields provided"),
+            404: OpenApiResponse(description="Photo with given id not found")
+        },
+    )
     def put(self, request, *args, **kwargs):
         if request.data is None:
             return Response(status=status.HTTP_400_BAD_REQUEST)
@@ -67,6 +98,17 @@ class SinglePhotoView(BaseView):
         data = PhotoSerializer(outcome.result).data
         return Response(data, status=status.HTTP_200_OK)
 
+    @extend_schema(
+        summary="Schedule Photo's delayed deletion.",
+        parameters=[
+            OpenApiParameter(name="photo_id", location="path", type=int, description="id of Photo to be deleted"),
+        ],
+        responses={
+            202: OpenApiResponse(description="Operation scheduled"),
+            404: OpenApiResponse(description="Photo with given id not found"),
+            410: OpenApiResponse(description="Photo already set to be deleted")
+        },
+    )
     def delete(self, request, *args, **kwargs):
         photo = self._get_photo_with_permission_check()
         outcome = ServiceOutcome(
