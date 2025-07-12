e7ad2e12f0e20420692c0f5c743ba777d5ddc3b9
bibrikthesorcerer <ignatsahovsky@gmail.com>
2025-07-12 17:31:50 +0300

    changed username to email in terms of social_auth pipeline get_or_create, added unusable password

--- main_app/pipeline.py
+++ main_app/pipeline.py
@@ -1,6 +1,9 @@
 from models_app.models import UserProfile
 
 def get_or_create_user(strategy, details, backend, user=None, *args, **kwargs):
-    username = details.get("username", kwargs.get("username"))
-    user, is_new = UserProfile.objects.get_or_create(username=username, defaults=details)
+    email = details.get("email", kwargs.get("email"))
+    user, is_new = UserProfile.objects.get_or_create(email=email, defaults=details)
+    if is_new:
+        user.set_unusable_password()
+        user.save()
     return {"is_new": bool(is_new), "user": user}
