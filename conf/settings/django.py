89b9edda4bb0d02187e587c0a13f9ae19a03dbc9
bibrikthesorcerer <ignatsahovsky@gmail.com>
2025-07-28 19:43:51 +0300

    fixes: form inputs required, mail tls

--- conf/settings/django.py
+++ conf/settings/django.py
@@ -121,6 +121,7 @@ EMAIL_PORT = config("EMAIL_PORT")
 EMAIL_HOST = config("EMAIL_HOST")
 EMAIL_HOST_USER = config("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD")
+EMAIL_USE_TLS = True
 
 # Password validation
 
